2454ca7a41fffad0599b74909d35f816647f53ba
Mateusz Maci±g <mateo.m@wp.pl>
2023-02-13 21:36:25 +0100

    Start

---
+++ src/main/java/module-info.java
@@ -0,0 +1,14 @@
+module com.sm.smarthome {
+    requires javafx.controls;
+    requires javafx.fxml;
+    requires javafx.web;
+
+    requires org.controlsfx.controls;
+    requires com.dlsc.formsfx;
+    requires net.synedra.validatorfx;
+    requires org.kordamp.ikonli.javafx;
+    requires org.kordamp.bootstrapfx.core;
+
+    opens com.sm.smarthome to javafx.fxml;
+    exports com.sm.smarthome;
+}
